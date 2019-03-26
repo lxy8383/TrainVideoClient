4376693200ab6db9fa6eaaae58620a04f5ab5791
Liuxy <liuxya@suning-sports.com>
2019-03-26 18:02:00 +0800

    Initial Commit

---
+++ LXYNetTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXYNetTest
+//
+//  Created by liuxy on 2019/3/26.
+//  Copyright © 2019年 liuxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXYNetTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXYNetTest
+//
+//  Created by liuxy on 2019/3/26.
+//  Copyright © 2019年 liuxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
