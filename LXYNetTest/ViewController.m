be934c4362425ad36282e4643d148591e56b727f
Liuxy <liuxya@suning-sports.com>
2019-04-02 15:08:47 +0800

    创建项目

--- LXYNetTest/ViewController.m
+++ LXYNetTest/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <AFNetworking.h>
 @interface ViewController ()
 
 @end
@@ -16,8 +16,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(50, 50, 80, 40);
+    [btn setTitle:@"点击" forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor redColor];
+    [btn addTarget:self action:@selector(netAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
 }
 
+- (void)netAction:(UIButton *)sender
+{
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.requestSerializer= [AFHTTPRequestSerializer serializer];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/javascript",@"text/plain",@"text/html", nil];
+    NSString *URLStr = @"http://localhost:9090/";
+    [manager GET:URLStr parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    }success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        NSLog(@"这里打印请求成功要做的事");
+         
+    }failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull   error) {
+        
+        
+         NSLog(@"%@",error);  //这里打印错误信息
+         
+    }];
 
+}
 @end
